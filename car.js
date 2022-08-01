ec3c391e714a041e3a3aeafa93eb327de0895820
Ilenia <nethleen@gmail.com>
2022-08-01 08:30:05 +0000

    left and right movement

--- car.js
+++ car.js
@@ -36,6 +36,14 @@ class Car {
     if (Math.abs(this.speed) < this.friction) {
       this.speed = 0;
     }
+
+    if (this.controls.left) {
+      this.x += 2;
+    }
+    if (this.controls.right) {
+      this.x -= 2;
+    }
+
     this.y -= this.speed;
   }
   draw(ctx) {
